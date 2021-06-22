951db12f7969a26ff003812730ddeb697f9ee394
valdofal <v.dofal@gmail.com>
2021-06-22 20:09:49 +0200

    storing game data to games.json should be done, for all different games

--- scoreBoard.ts
+++ scoreBoard.ts
@@ -1,13 +1,42 @@
-const fs = require('fs')
+import { readFileSync, writeFile } from 'fs';
+
 function scoreBoardToJSON(scoreBoard:Object,  start:Date, end: Date, game:string ){
-    console.log(scoreBoard)
-    let Object = 
-        {
-            "beg":start,
-            "end":end,
-            "players":scoreBoard
+    let dataToAppend = datasToJSONFormat(scoreBoard,start,end,game) //Format the data we want to add to be able to add it
+    let file = readFileSync('games.json')
+    let actualData
+    if(file.length === 0){   // The file is empty, we can't parse the JSON in it, it would crash
+        actualData = {[game] : [dataToAppend]}
+    }
+    else{
+        actualData = JSON.parse(readFileSync('games.json').toString())  // Read the data we have in the file
+        if(!actualData[`${game}`]){                                         //If there is no data for the game in question, we create an array
+            actualData[`${game}`] = [dataToAppend]
         }
-    fs.writeFileSync('./games.json',"{\""+game+"\":["+JSON.stringify(Object)+"]}")
+        else{                                                               //If there already is data, we append it to the corresponding index
+            actualData[`${game}`][actualData[`${game}`].length] = dataToAppend
+        }
+    }   
+
+    writeFile('games.json',JSON.stringify(actualData,null,4),(err) => { //Finally we write the data to the file, erasing the previous content to make sure we dont have json syntax errors
+        if(err) throw err
+        console.log("Game has been saved to games.json")
+    })
+
+}
+
+
+function datasToJSONFormat(scoreBoard:Object,  start:Date, end: Date, game:string ):string{
+    let chaine = `
+        {
+            "beg":"${start}",
+            "end":"${end}",
+            "players":[
+                ${Object.entries(scoreBoard).map((key)=> {
+                    return `{"name": "${key[0]}", "points":${key[1]}}`
+                })}
+            ]
+}`
+    return JSON.parse(chaine)
 }
 
 export default scoreBoardToJSON
--- server.ts
+++ server.ts
@@ -9,9 +9,6 @@ let dateStart:Date = new Date(0)
 function createMagicNumber():number{
   return Math.floor(Math.random() * 1338)
 }
-console.log(MagicNumber)
-
-
 
 const app = require('express')();
 const server = require('http').createServer(app);
@@ -29,6 +26,7 @@ server.listen(process.env.PORT ||3001, () => {
 io.on('connection', (socket: any) => {
   console.log('client connected')          //Listening for any new connections
   socket.on('number', (msg: any)  => {     //Listening for any new number submitted by user
+    console.log(`user ${msg.username} submitted number ${msg.number}`)
     if(!scoreBoard[msg.username]){
       scoreBoard[msg.username] = 0
     }
@@ -37,10 +35,11 @@ io.on('connection', (socket: any) => {
     }
     if(msg.number == MagicNumber){         //If number is the right one then ...
       if(scoreBoard[msg.username] === 2){  //If he has 2 wins already, means he won the game
+        console.log(`game over : ${msg.username} won the game`)
         scoreBoard[msg.username] ++
         io.sockets.emit('gameOver',{scoreBoard: scoreBoard, username:msg.username}) //We send info to clients that game is over and with the score board
         //Add save to json FILE of game state
-        // scoreBoardToJSON(scoreBoard, dateStart , new Date(Date.now()),'magicNumber') // need to fix nodemon
+        scoreBoardToJSON(scoreBoard, dateStart , new Date(Date.now()),'magicNumber') // need to fix nodemon
         scoreBoard = {}
       }
       else{    
